238931f5488207146854f7a0fb851b97ebff48b2
farooq-teqniqly <farooq@teqniqly.com>
2024-10-04 22:08:15 -0700

    Refactor math module usage and enhance error handling
    Replaced math module calls with numpy to streamline dependencies. Added response.raise_for_status() for better error handling in download function. Adjusted get_normalized_wp logic to handle missing values more robustly, and fixed load function call to use Path object.

--- winprob.py
+++ winprob.py
@@ -3,13 +3,14 @@ from pathlib import Path
 from typing import List, Dict
 import numpy as np
 import requests
-import math
 
 def download(url: str) -> str:
     response = requests.get(url)
 
     if response.status_code == 200:
         return response.text
+
+    response.raise_for_status()
 
 def save(response_text: str, target_file_path: Path) -> None:
     with open(target_file_path, "w", encoding="utf-8") as file:
@@ -28,7 +29,7 @@ def parse_raw_wp_vals(content: str) -> List[int]:
         rect_element = path_element.find_previous_sibling("rect")
 
         if rect_element and rect_element.has_attr("y"):
-            raw_wp_vals.append(math.floor(float(rect_element["y"])))
+            raw_wp_vals.append(np.floor(float(rect_element["y"])))
 
     return raw_wp_vals
 
@@ -39,19 +40,20 @@ def get_interpolation() -> Dict[int, int]:
     percentages = np.arange(0, 101, 1)
     interpolation = np.interp(percentages, norm_points, raw_points)
 
-    return dict(zip([math.floor(i) for i in interpolation], percentages))
+    return dict(zip([np.floor(i) for i in interpolation], percentages))
 
 def get_normalized_wp(raw_wp_vals: List[int], interpolation: dict) -> List[int]:
     normalized_vals = []
 
     for v in raw_wp_vals:
-        normalized_val: int
+        if v not in interpolation:
+            while v not in interpolation:
+                v -= 1
+                if v < 0:
+                    v = 0
+                    break
 
-        if not v in interpolation:
-            normalized_val = interpolation[v - 1]
-        else:
-            normalized_val = interpolation[v]
-
+        normalized_val = interpolation[v]
         normalized_vals.append(normalized_val)
 
     return normalized_vals
@@ -60,7 +62,7 @@ if __name__ == "__main__":
     # url = "https://www.baseball-reference.com/boxes/MIL/MIL202410030.shtml"
     # response_text = download(url)
     # save(response_text, Path("python.html"))
-    content = load("page_content.html")
+    content = load(Path("page_content.html"))
     raw_wp_vals = parse_raw_wp_vals(content)
     interpolation = get_interpolation()
     normalized_wp_vals = get_normalized_wp(raw_wp_vals, interpolation)
