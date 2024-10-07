2efe6e9cde4bfe48999992299464522942752805
farooq-teqniqly <farooq@teqniqly.com>
2024-10-07 10:54:47 -0700

    Add CLI and progress indicators to download script
    Introduced CLI functionality using argparse for more flexible usage, and integrated tqdm for visual progress indicators during downloads. Enhanced the existing tests to support dynamic file naming.

--- download.py
+++ download.py
@@ -1,8 +1,10 @@
+import argparse
 import json
 import os
 import time
 from pathlib import Path
 from typing import List
+from tqdm import tqdm
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -28,7 +30,7 @@ def download_boxscore_links(year: int) -> str:
 
         boxscore_links = []
 
-        for a_tag in a_tags:
+        for a_tag in tqdm(a_tags):
             if a_tag.get_text() == "Boxscore":
                 boxscore_links.append(f"{ROOT_URL}{a_tag["href"]}")
 
@@ -46,10 +48,11 @@ def download_box_scores(links: List[str], output_dir: Path, delay_between_downlo
         output_dir: Directory where the downloaded box scores will be saved.
         delay_between_downloads_seconds: Number of seconds to wait between downloads.
     """
-    for link in links:
+    for link in tqdm(links):
         filename = os.path.join(output_dir, f"{link.split('/')[-1]}")
 
         if os.path.exists(filename):
+            print(f"Skipping {filename}")
             continue
 
         soup = _get_soup_for_page(link)
@@ -79,3 +82,59 @@ def _get_soup_for_page(url: str) -> BeautifulSoup:
     driver.get(url)
     _scroll_to_bottom()
     return BeautifulSoup(driver.page_source, "html.parser")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="CLI tool for downloading links and boxes.")
+
+    subparsers = parser.add_subparsers(dest="command", required=True, help="Commands")
+
+    download_links_parser = subparsers.add_parser("download-links",
+                                                  help='Download links for a given year and target directory')
+    download_links_parser.add_argument(
+        "--year", required=True, type=int, help="Year for which to download links")
+
+    download_links_parser.add_argument("--target-dir", required=True, type=str,
+                                       help="Target directory to save the links")
+
+    download_boxes_parser = subparsers.add_parser("download-boxes", help="Download boxes from a links file")
+
+    download_boxes_parser.add_argument("--links-file", required=True, type=str,
+                                       help="File containing the download links")
+
+    download_boxes_parser.add_argument("--target-dir", required=True, type=str,
+                                       help="Target directory to save the boxscores.")
+
+    args = parser.parse_args()
+
+    if args.command == "download-links":
+        boxscore_links_json = download_boxscore_links(args.year)
+        _write_boxscores_to_target(args, boxscore_links_json)
+
+    elif args.command == "download-boxes":
+        with open(args.links_file, "r", encoding="utf-8") as file:
+            links = json.load(file)
+            output_dir = _create_boxscore_target_dir(args)
+            download_box_scores(links, output_dir)
+
+
+def _create_boxscore_target_dir(args):
+    year = args.links_file.replace(".json", "").split("_")[-1]
+    output_dir = os.path.join(args.target_dir, "boxscores", str(year))
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    return output_dir
+
+
+def _write_boxscores_to_target(args, boxscore_links_json):
+    if not os.path.exists(args.target_dir):
+        os.makedirs(args.target_dir)
+
+    with open(os.path.join(args.target_dir, f"boxscore_links_{args.year}.json"), "w", encoding="utf-8") as file:
+        file.write(boxscore_links_json)
+
+    print("Boxscore links has been saved successfully.")
+
+
+if __name__ == "__main__":
+    main()
--- test/test_download_boxscore.py
+++ test/test_download_boxscore.py
@@ -3,21 +3,22 @@ import os.path
 
 from download import download_boxscore_links, download_box_scores
 
+year = 2023
+filename = f"boxscore_links_{year}.json"
 
 def test_can_download_and_save_boxscore_links():
-    year = 2024
     boxscore_links_json = download_boxscore_links(year)
 
-    with open("boxscore_links_2024.json", "w", encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(boxscore_links_json)
 
     print("Boxscore links has been saved successfully.")
 
 
 def test_can_download_boxscores():
-    with open("boxscore_links_2024.json", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         links = json.load(file)
-        output_dir = os.path.join(os.getcwd(), "boxscores")
+        output_dir = os.path.join(os.getcwd(), "boxscores", str(year))
 
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
